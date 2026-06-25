e522bf5042bc269929aa1d6ce12e7ab35080bca1
FelipeBrasill <felipebrasil567@gmail.com>
2026-06-24 21:49:43 -0300

    feat: criação da classe jogador

--- uno/backend/models/jogador.py
+++ uno/backend/models/jogador.py
@@ -1,10 +1,26 @@
-from uno.backend.models.carta import Carta
+from uno.backend.models.baralho import Baralho
 from uno.backend.models.mao import Mao
+from uno.backend.models.carta import Carta
+
 class Jogador:
-    '''Clase que representa un jugador de UNOKanBAN.'''
     def __init__(self, nome: str):
-        self.nome : str = nome
+        self._nome : str = nome
         self._mao : Mao = Mao()
-    def comprar_carta(self, carta: Carta):
-        '''Adiciona uma carta à mão do jogador.'''
-        self._mao.appen
+        self._flag_desistiu : bool = False
+
+    def comprar_carta(self, baralho: Baralho) -> None:
+        '''Permite que o jogador compre uma carta do baralho.'''
+        carta = baralho.retirar_carta()
+        self._mao.adicionar_carta(carta)
+    
+    def desistir(self) -> None:
+        '''Permite que o jogador desista da partida.'''
+        self._flag_desistiu = True
+    
+    def passar_vez(self) -> None:
+        '''Permite que o jogador passe a vez.'''
+
+    def jogar_carta(self, carta: Carta) -> None:
+        '''Permite que o jogador jogue uma carta da sua mão.'''
+        self._mao.remover_carta(carta)
+    
