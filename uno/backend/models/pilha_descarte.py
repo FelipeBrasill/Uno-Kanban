314483eb0b7e6c4553245559598de88e3881f042
FelipeBrasill <felipebrasil567@gmail.com>
2026-06-27 23:35:34 -0300

    refactor refactor: remoção de método descarte da pilha de descarte

--- uno/backend/models/pilha_descarte.py
+++ uno/backend/models/pilha_descarte.py
@@ -21,12 +21,6 @@ class PilhaDescarte:
         topo_descarte = self._cartas[-1]
         return topo_descarte
     
-    def retirar_carta(self) -> Carta:
-        '''Retira uma carta do topo da pilha de descarte.'''
-        if not self._cartas:
-            raise ValueError('A pilha de descarte está vazia.')
-        return self._cartas.pop()
-    
     def adicionar_carta(self, carta: Carta) -> None:
         '''Adiciona uma carta ao topo da pilha de descarte.'''
         self._cartas.append(carta)
