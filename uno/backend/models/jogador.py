0b71cd2fc1001346617233be591631fdb3358dbb
FelipeBrasill <felipebrasil567@gmail.com>
2026-06-27 23:46:28 -0300

    refactor: melhoria na independencia e divisao de responsabilidades entre as interacoes com jogador

--- uno/backend/models/jogador.py
+++ uno/backend/models/jogador.py
@@ -2,32 +2,38 @@ from uno.backend.models.baralho import Baralho
 from uno.backend.models.mao import Mao
 from uno.backend.models.carta import Carta
 from uno.backend.models.config import QTD_COMPRA_PADRAO
+from uno.backend.models.enum import EstadoJogador, EstadoRealiEhGay
 class Jogador:
     def __init__(self, nome: str):
-        self.nome : str = nome
-        self._mao : Mao = Mao()
-        self._flag_desistiu : bool = False
+        self.nome: str = nome
+        self._mao: Mao = Mao()
+        self._flag_desistiu: bool = False
+        self.estado_realiehgay : EstadoRealiEhGay  = EstadoRealiEhGay.NORMAL
 
-    def comprar_carta(self, baralho: Baralho, quantidade : int = QTD_COMPRA_PADRAO ) -> None:
-        '''Permite que o jogador compre uma carta do baralho.'''
-        for _ in range(quantidade):    
+    def comprar_carta(self, baralho: Baralho, quantidade: int = QTD_COMPRA_PADRAO) -> Carta | None:
+        '''Permite comprar n cartas.'''
+        carta = None
+        for _ in range(quantidade):
             carta = baralho.retirar_carta()
             self._mao.adicionar_carta(carta)
-        
-    def desistir(self) -> None:
-        '''Permite que o jogador desista da partida.'''
-        self._flag_desistiu = True
-    
-    def passar_vez(self) -> None:
-        '''Permite que o jogador passe a vez.'''
 
-    def jogar_carta(self, carta: Carta) -> Carta:
-        '''Permite que o jogador jogue uma carta da sua mão.'''
-        # jogador seleciona uma carta da mão 
+        if quantidade == QTD_COMPRA_PADRAO:
+            return carta
+        return None
+
+    def jogar_carta(self, carta: Carta) -> None:
+        '''Remove a carta da mão do jogador.'''
         self._mao.remover_carta(carta)
-        # carta é adicionada na pilha
-        self.adicionar_carta(carta)
-        return carta
-    
-    
-    
+
+    def desistir(self) -> None:
+        '''Marca o jogador como desistente.'''
+        self._flag_desistiu = True
+
+    @property
+    def mao_vazia(self) -> bool:
+        '''Retorna True se o jogador não tem mais cartas.'''
+        return self.quantidade_cartas_mao() == 0
+
+    @property
+    def quantidade_cartas_mao(self)-> int:
+        return self._mao.quantidade()
